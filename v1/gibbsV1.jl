6bf99f2fcd65bb7037a39c2c3fef033424d73216
Ferdinand Willemin <ferdi.willemin@gmail.com>
2024-06-15 13:11:08 -0400

    Add function signatures in 'gibbsV1.jl'

--- v1/gibbsV1.jl
+++ v1/gibbsV1.jl
@@ -3,32 +3,43 @@ using Distributions: loglikelihood
 include("metropolis.jl")
 include("GMRF.jl")
 
+"""
+    gibbs(niter, y; δ², κᵤ₀, μ₀, W)
+
+Perform bayesian inference on the hierarchical model's parameters through the Gibbs algorithm.
+
+Generation of the GEV's location parameter is performed through a one-iteration Metropolis algorithm.
+Print the acceptation rate of the GEV's location parameter for each grid cell.
+
+# Arguments
+
+- `niter::Integer`: The number of iterations for the gibbs algorithm.
+- `y::Array{Float64,3}`: Three-dimension array containing the observations of each grid cell.
+- `δ²::Real`: Instrumental variance for the one-iteration Metropolis algorithm.
+- `κᵤ₀::Real`: Initial value of the inferred GMRF's precision parameter.
+- `μ₀::Vector{<:Real}`: Initial values of the inferred GEV's location parameter.
+- `W::SparseMatrixCSC`: Structure matrix of the inferred GMRF.
+"""
 function gibbs(niter::Integer, y::Array{Float64,3}; δ²::Real, κᵤ₀::Real, μ₀::Vector{<:Real}, W::SparseMatrixCSC)
 
-    # Nombre de cellules
     m = length(μ₀)
-    # Redimensionnement des données
     nobs = size(y, 3)
     y = reshape(y, m, nobs)
 
-    # Initialisation
     κᵤ = zeros(niter)
     κᵤ[1] = κᵤ₀
     μ = zeros(m, niter)
     μ[:, 1] = μ₀
 
-    # Taux d'acceptation des μ
     acc = falses(m, niter)
 
-    # Itérations
     for i = 2:niter
-        # On génère les μᵢ | κᵤ, μ₋ᵢ
+        # Generate μᵢ | κᵤ, μ₋ᵢ
         for k = 1:m
             μ[k, i], acc[k, i] = updateμₖ(k, i, μ, δ², y[k, :], κᵤ[i-1])
         end
-        # On génère κᵤ
-        μ̄ᵢ = neighborsMutualEffect(W, μ[:, i])
-        κᵤ[i] = rand(fcκᵤ(W, μ[:, i], μ̄ᵢ))
+        # Generate κᵤ
+        κᵤ[i] = rand(fcκᵤ(μ[:, i], W=W))
     end
 
     accRates = count(acc, dims=2) ./ (niter - 1) .* 100
@@ -39,22 +50,32 @@ function gibbs(niter::Integer, y::Array{Float64,3}; δ²::Real, κᵤ₀::Real, 
     return κᵤ, μ
 end
 
+
 """
-Tentative de réorganisaion des itérations --> mêmes résultats...
+    updateμₖ(k, i, μ, δ², y, κᵤ)
+
+Update the inferred GEV's location parameter at iteration i of the gibbs algorithm.
+
+Consist in a one-iteration Metropolis algorithm.
+
+# Arguments
+
+- `k::Integer`: Numero of the current cell.
+- `i::Integer`: Numero of the current gibbs iteration.
+- `μ::Matrix{<:Real}`: Current trace of the location parameter for each grid cell.
+- `δ²::Real`: Instrumental variance.
+- `y::Vector{<:Real}`: Observations for cell k.
+- `κᵤ::Real`: Last updated value of the precision parameter.
 """
 function updateμₖ(k::Integer, i::Integer, μ::Matrix{<:Real}, δ²::Real, y::Vector{<:Real}, κᵤ::Real)
-    # On génère un candidat pour Ui
-    # Et donc pour μ
+
     μ̃ = rand(Normal(μ[k, i-1], δ²))
-    # État précédent
     μ₀ = μ[k, i-1]
-    # Calcul de la différence de log-vraisemblance au niveau des données
-    logL = loglikelihood(GeneralizedExtremeValue(μ̃, 1.0, 0.0), y) - loglikelihood(GeneralizedExtremeValue(μ₀, 1.0, 0.0), y)
-    # Calcul de la différence de log-vraisemblance au niveau latent
-    lf = logpdf(fcIGMRF(k, i, κᵤ, W, μ), μ̃) - logpdf(fcIGMRF(k, i, κᵤ, W, μ), μ[k, i-1])
-    # Somme des deux différences
+
+    logL = datalevelloglike(μ̃, y) - datalevelloglike(μ₀, y)
+    lf = latentlevelloglike(k, i, μ̃; κᵤ=κᵤ, W=W, μ=μ) - latentlevelloglike(k, i, μ₀; κᵤ=κᵤ, W=W, μ=μ)
     lr = logL + lf
-    # Acceptation
+
     if lr > log(rand())
         return μ̃, true
     else
@@ -62,45 +83,109 @@ function updateμₖ(k::Integer, i::Integer, μ::Matrix{<:Real}, δ²::Real, y::
     end
 end
 
+
 """
-Loi conditionnelle complète du iGMRF
+    datalevelloglike(μ, y)
+
+Compute the log-likelihhod at the data level evaluated at `μ` knowing the observations `y`.
+
+# Arguments
+
+- `μ::Real`: Location parameter.
+- `y::Vector{<:Real}`: Observations.
 """
-function fcIGMRF(k::Integer, i::Integer, κᵤ::Real, W::SparseMatrixCSC, μ::Matrix{<:Real})
+function datalevelloglike(μ::Real, y::Vector{<:Real})
+
+    return loglikelihood(GeneralizedExtremeValue(μ, 1.0, 0.0), y)
+
+end
+
+
+"""
+    latentlevelloglike(k, i, value; κᵤ, W, μ)
+
+Compute the log-likelihhod at the latent level evaluated at `value` knowing the neighbors' location parameter.
+
+# Arguments
+
+- `k::Integer`: Numero of the current cell.
+- `i::Integer`: Numero of the current gibbs iteration.
+- `value`: Value for which the log-likelihood is evaluated.
+- `κᵤ::Real`: Last updated value of the precision parameter.
+- `W::SparseMatrixCSC`: Structure matrix of the inferred GMRF.
+- `μ::Matrix{<:Real}`: Last updated trace of the location parameter for each grid cell.
+"""
+function latentlevelloglike(k::Integer, i::Integer, value::Real; κᵤ::Real, W::SparseMatrixCSC, μ::Matrix{<:Real})
+    return logpdf(fcIGMRF(k, i, κᵤ=κᵤ, W=W, μ=μ), value)
+end
+
+
+"""
+    fcIGMRF(k, i; κᵤ, W, μ)
+
+Compute the probability density of the full conditional function of the GEV's location parameter due to the iGMRF.
+
+# Arguments
+
+- `k::Integer`: Numero of the current cell.
+- `i::Integer`: Numero of the current gibbs iteration.
+- `κᵤ::Real`: Last updated value of the precision parameter.
+- `W::SparseMatrixCSC`: Structure matrix of the inferred GMRF.
+- `μ::Matrix{<:Real}`: Last updated trace of the location parameter for each grid cell.
+"""
+function fcIGMRF(k::Integer, i::Integer; κᵤ::Real, W::SparseMatrixCSC, μ::Matrix{<:Real})
 
     # μ partially updated
     μUpdated = vcat(μ[1:k, i], μ[k+1:end, i-1])
 
-    # Structure matrix without its diagonal
     W̄ = W - spdiagm(diag(W))
 
-    # Parameters of the canonical Normal
     Q = κᵤ * Array(diag(W))
     b = -κᵤ * (W̄ * μUpdated)
 
-    pd = NormalCanon(b[k], Q[k])
-
-    return pd
+    return NormalCanon(b[k], Q[k])
 
 end
 
+
 """
-Distribution conditionnelle complète pour κᵤ.
+    fcκᵤ(μ; W)
+
+Compute the probability density of the full conditional function of the iGMRF's precision parameter.
+
+# Arguments
+
+- `μ::Matrix{<:Real}`: Last updated value of the location parameter for each grid cell.
+- `W::SparseMatrixCSC`: Structure matrix of the inferred GMRF.
 """
-function fcκᵤ(W::SparseMatrixCSC, μ::Vector{<:Real}, μ̄::Vector{<:Real})
+function fcκᵤ(μ::Vector{<:Real}; W::SparseMatrixCSC)
+
+    μ̄ = neighborsMutualEffect(W, μ)
+
     m = size(μ, 1)
     α = m / 2 + 1
     β = sum(dot(diag(W), (μ .- μ̄) .^ 2)) / 2 + 1 / 100
+
     return Gamma(α, 1 / β)
+
 end
 
+
 """
-Calcul des effets mutuels des voisins.
+    neighborsMutualEffect(W, u)
+
+Compute the neighbors effect's term for each grid cell at the same time.
+
+# Arguments
+
+- `W::SparseMatrixCSC`: Structure matrix of the inferred GMRF.
+- `μ::Vector{<:Real}`: Value of the location parameter for each grid cell.
 """
-function neighborsMutualEffect(W::SparseMatrixCSC, u::Vector{<:Real})
-    # Annule la diagonale de W⁻
+function neighborsMutualEffect(W::SparseMatrixCSC, μ::Vector{<:Real})
+
     W⁻ = W - spdiagm(diag(W))
-    # Somme pondérée par les uⱼ
-    W⁻u = W⁻ * u
-    # Multiplication par l'inverse de la diagonale de W
-    return -spdiagm(1 ./ diag(W)) * W⁻u
+    W⁻μ = W⁻ * μ
+
+    return -spdiagm(1 ./ diag(W)) * W⁻μ
+
 end
