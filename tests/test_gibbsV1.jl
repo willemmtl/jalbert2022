57d7afb5887ffd0db5bd730de894dde40e7776d4
Ferdinand Willemin <ferdi.willemin@gmail.com>
2024-06-15 13:14:53 -0400

    redefine folder structure

---
+++ tests/test_gibbsV1.jl
@@ -0,0 +1,40 @@
+using Test, Distributions
+
+include("../gibbs.jl");
+include("../GMRF.jl");
+
+@testset "gibbs.jl" begin
+
+    @testset "neighborsEffect(i, k, W, μ)" begin
+
+        W = buildStructureMatrix(2, 2)
+
+        μ = zeros(4, 3)
+        μ[:, 1] = ones(4)
+        μ[1, 2] = 2
+        μ[2, 2] = 3
+
+        @test neighborsEffect(2, 3, W, μ) ≈ 3 / 2
+
+    end
+
+    @testset "neighborsMutualEffect(W, μ)" begin
+
+        W = buildStructureMatrix(2, 2)
+
+        μ = [1, 2, 3, 4]
+
+        @test neighborsMutualEffect(W, μ) == [2.5, 2.5, 2.5, 2.5]
+
+    end
+
+    @testset "fcκᵤ(W, μ)" begin
+
+        W = buildStructureMatrix(2, 2)
+        μ = ones(4)
+
+        @test fcκᵤ(W, μ) == Gamma(3, 100 / 401)
+
+    end
+
+end
