6fd99f523cb83a7f4a42ac15bf6ee18108602411
Ferdinand Willemin <ferdi.willemin@gmail.com>
2024-07-20 10:57:48 -0400

    feat(evaluation): m1 and m2 are now parameters

---
+++ evaluation.jl
@@ -0,0 +1,43 @@
+using Statistics
+
+include("iGMRF.jl");
+include("dataGen.jl");
+include("gibbs.jl");
+
+function evaluate(N; nobs::Integer, niter::Integer, m₁::Integer, m₂::Integer)
+
+    Random.seed!(400);
+
+    m = m₁ * m₂;
+    κᵤ = 100.0;
+    F = iGMRF(m₁, m₂, κᵤ);
+    grid_target = generateTargetGrid(F);
+    data = generateData(grid_target, nobs);
+
+    niter = 1000;
+    δ² = 0.07;
+    κᵤ₀ = 10;
+    μ₀ = zeros(m);
+
+    κ̂ᵤ = zeros(N);
+    distances = zeros(N);
+    times = zeros(N);
+
+    for n=1:N
+        startTime = time()
+        κᵤ, μ = gibbs(niter, data, m₁=m₁, m₂=m₂, δ²=δ², κᵤ₀=κᵤ₀, μ₀=μ₀);
+        endTime = time()
+        times[n] = endTime - startTime
+
+        μ̂ = mean(μ, dims=2);
+        κ̂ᵤ[n] = mean(κᵤ);
+
+        distances[n] = norm(reshape(μ̂, m₁, m₂) .- grid_target[:, :, 1], 2) / m
+    end
+
+    println("κ̂ᵤ = ", mean(κ̂ᵤ))
+    println("Distance = ", mean(distances))
+    println("Temps d'exécution = ", mean(times))
+end
+
+evaluate(10, nobs=1000, niter=10000, m₁=10, m₂=10);
